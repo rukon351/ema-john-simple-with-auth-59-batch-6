54a943e6e18cdbf47bbe84fb604f4774f09319ca
Rukonuzzaman Shiem <rukonuzzaman.shiem.351@gmail.com>
2023-02-23 00:48:51 +0600

    added firebase & sign in signUp form

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh8Wlr85HHzOqr0NjqeiZFf-t_Dc4O24I",
+  authDomain: "ema-john-simple-27edc.firebaseapp.com",
+  projectId: "ema-john-simple-27edc",
+  storageBucket: "ema-john-simple-27edc.appspot.com",
+  messagingSenderId: "570782319948",
+  appId: "1:570782319948:web:b3e6039f8d2ee0b9e57be9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
