102b55990241256da65095a1ce76dfd085e995df
Rukonuzzaman Shiem <rukonuzzaman.shiem.351@gmail.com>
2023-03-30 14:21:27 +0600

    added all setup

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -23,15 +23,19 @@ import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../contexts/UserContext';
 
-const PrivateRoute = ({children}) => {
-    const {user} = useContext(AuthContext);
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
 
-   if(user && user.uid) {
-    return children;
-   }
-   return <Navigate to="/login" state={{ from: location }} replace />;
-   
+    if (loading) {
+        <div>Loading...</div>
+    }
+
+    if (user && user.uid) {
+        return children;
+    }
+    return <Navigate to="/login" state={{ from: location }} replace />;
+
 };
 
 export default PrivateRoute;
